206f2b259e942bfb0f4d4fc455725eda60afaa82
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-10-31 19:10:44 +0300

    Добавьте файлы проекта.

---
+++ Тема Наследование 30.10.22г/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тема_Наследование_30._10._22г
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cats cat = new Cats();
+            cat.Gender = "Кот";
+            cat.Breed = "Вислоухий";
+            cat.Color = "Серый";
+            cat.Age = 2;
+            Console.WriteLine(cat.Display());
+            Console.WriteLine(cat.Display2());
+            cat.Print();
+            Console.ReadKey();
+        }
+    }
+    public class Zoomagazin
+    {
+        public Zoomagazin(string gender, string breed, string color, byte age)
+        {
+            this.Gender = gender;
+            this.Breed = breed;
+            this.Color = color;
+            this.Age = age;
+        }
+
+        public Zoomagazin() { }
+        public string Gender { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public byte Age { get; set; }
+
+        public string Display()
+        {
+            return $"Животное: {Gender}";
+        }
+        public string Display2()
+        {
+            return $"Пол: {Gender}, Порода: {Breed}, Возраст: {Age}";
+        }
+    }
+    public class Cats : Zoomagazin
+    {
+        public void Print()
+        {
+            Console.WriteLine("Все животные домашние и абсолютно здоровые!");
+        }
+    }
+}
---
+++ Тема Наследование 30.10.22г/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Тема Наследование 30.10.22г")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Тема Наследование 30.10.22г")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6d3f2025-10f7-425b-9a6e-580082d1f10a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
